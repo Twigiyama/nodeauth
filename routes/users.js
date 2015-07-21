a4d6724b50e757448b48b468816d26db90ab791d
Twigiyama <asitha@tomorrows.company>
2015-07-21 22:53:09 +0100

    Added connection to MongoDB and persist login details to database

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -65,6 +67,7 @@ router.post('/register', function(req, res, next){
     } else {
         var newUser = new User({
             errors: errors,
+            name: name,
             email: email,
             username: username,
             password: password,
